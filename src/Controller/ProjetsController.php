7c220b226a32fa1f91bb7b2ae42d4d0bf72bda59
JAY <92600390+laxmiav@users.noreply.github.com>
2023-06-05 20:38:34 +0200

    final commit

---
+++ src/Controller/ProjetsController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Projets;
+use App\Entity\Tasks;
+use App\Form\ProjetsType;
+use App\Repository\ProjetsRepository;
+use App\Repository\TasksRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/projets')]
+class ProjetsController extends AbstractController
+{
+    #[Route('/', name: 'app_projets_index', methods: ['GET'])]
+    public function index(ProjetsRepository $projetsRepository): Response
+    {
+        return $this->render('projets/index.html.twig', [
+            'projets' => $projetsRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_projets_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, ProjetsRepository $projetsRepository): Response
+    {
+        $projet = new Projets();
+        $form = $this->createForm(ProjetsType::class, $projet);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $projetsRepository->save($projet, true);
+
+            return $this->redirectToRoute('app_projets_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('projets/new.html.twig', [
+            'projet' => $projet,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_projets_show', methods: ['GET'])]
+    public function show(Projets $projet): Response
+    {
+        return $this->render('projets/show.html.twig', [
+            'projet' => $projet,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_projets_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Projets $projet, ProjetsRepository $projetsRepository): Response
+    {
+        $form = $this->createForm(ProjetsType::class, $projet);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $projetsRepository->save($projet, true);
+
+            return $this->redirectToRoute('app_projets_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('projets/edit.html.twig', [
+            'projet' => $projet,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_projets_delete', methods: ['POST'])]
+    public function delete(Request $request, Projets $projet, ProjetsRepository $projetsRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$projet->getId(), $request->request->get('_token'))) {
+            $projetsRepository->remove($projet, true);
+        }
+
+        return $this->redirectToRoute('app_projets_index', [], Response::HTTP_SEE_OTHER);
+    }
+// to find all releted tasks
+ /**
+     * @Route("/tasks/{id}", name="projets_tasks", methods={"GET"})
+     */
+    public function tasksindex(int $id,TasksRepository $tasksRepository): Response
+    {
+        return $this->render('tasks/index.html.twig', [
+            'tasks' => $tasksRepository->findTasksByProjects($id),
+        ]);
+    }
+
+
+
+}
---
+++ src/Form/ProjetsType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Projets;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProjetsType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('Description')
+            ->add('Starting_date')
+            ->add('Deadline_date')
+            ->add('user')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Projets::class,
+        ]);
+    }
+}
